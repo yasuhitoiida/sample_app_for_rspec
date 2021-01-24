bd8079dc69ebc29358ab8a248de13a6a3dde2e14
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-01-24 12:49:07 +0900

    taskモデルのバリデーションを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    title { "making a cake" }
+    status { 0 }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with all attributes' do
+      expect(FactoryBot.build(:task)).to be_valid
+    end
+
+    it 'is invalid without title' do
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it 'is invalid without status' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+
+    context 'when making a cake task already exists' do
+      before do
+        FactoryBot.create(:task)
+      end
+
+      it 'is invalid with a duplicate title' do
+        task = FactoryBot.build(:task)
+        task.valid?
+        expect(task.errors[:title]).to include("has already been taken")
+      end
+
+      it 'is valid with another title' do
+        task = FactoryBot.build(:task, title: 'writing a letter')
+        expect(task).to be_valid
+      end
+    end
+  end
 end
