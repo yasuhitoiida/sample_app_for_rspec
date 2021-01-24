47787a305113818d0ac0e89d42f70f21c66e6909
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-01-24 13:13:45 +0900

    Fix:userファクトリにシーケンスを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "example@runteq.org" }
+    sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
   end
